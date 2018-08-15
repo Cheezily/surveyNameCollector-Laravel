bade34c0865b83169693529bb0cf4c02f14f29d6
Cheezily <pm655@yahoo.com>
2018-08-15 18:29:26 -0500

    make analytics code be served from the database

--- resources/views/layouts/partials/_head.blade.php
+++ resources/views/layouts/partials/_head.blade.php
@@ -15,4 +15,20 @@
 
   <!-- Jquery -->
   <script src="{{ asset('js/jquery-3.2.1.slim.min.js') }}" type="text/javascript"></script>
+
+  <?php $analytics = App\Option::find(1)->google_analytics_key; ?>
+
+  @if(!empty($analytics))
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', '{{ $analytics }}', 'auto');
+    ga('send', 'pageview');
+
+  </script>
+  @endif
+
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -50,17 +50,6 @@
       </div>
     </body>
 
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-92837500-2', 'auto');
-    ga('send', 'pageview');
-
-  </script>
-
   <script type="text/javascript">
     var name1Output = document.getElementById('name1');
     name1Output.innerHTML = "<a href='mailto:philip." + "michaels@" + "gmail.com?Subject=Question Regarding ExtraCredit.Info'>"
